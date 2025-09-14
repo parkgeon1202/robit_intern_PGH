2250e1e03c1c3949bd7a2422c473c150fd8020ab
parkgeon1202 <geonhupark3436@gmail.com>
2025-09-14 12:08:40 +0900

    Delete ROS2/day1/hw3/pkg_hw3_cpp directory

--- ROS2/day1/hw3/pkg_hw3_cpp/src/publish.cpp
+++
@@ -1,8 +0,0 @@
-#include "hw3.hpp"
-int main(int argc, char * argv[]){
-    rclcpp::init(argc,argv);
-    auto node = std::make_shared<publisher>();
-    node->publish_message();
-    rclcpp::shutdown();
-    return 0;
-}
