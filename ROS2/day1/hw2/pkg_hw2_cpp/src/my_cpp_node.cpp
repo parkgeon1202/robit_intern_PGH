f5470cd07d4ea3c82c3c3fc5b970a52bfcfa20e5
parkgeon1202 <geonhupark3436@gmail.com>
2025-09-14 09:04:18 +0900

    Delete ROS2/day1/hw2/pkg_hw2_cpp directory

--- ROS2/day1/hw2/pkg_hw2_cpp/src/my_cpp_node.cpp
+++
@@ -1,9 +0,0 @@
-#include "pkg_hw2.hpp"
-int main(int argc, char **argv)
-{
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<MyCppNode>();
-    rclcpp::spin(node);
-    rclcpp::shutdown();
-    return 0;
-}
